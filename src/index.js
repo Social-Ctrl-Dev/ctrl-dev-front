91e04f029fb964577d2102d6cb6e731d8cb5ef7a
monte <montenegrojds@gmail.com>
2023-02-05 00:25:41 -0500

    Usando rutas para poder entrar a los post por ID

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import { FullPost } from './components/FullPost';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import App from './App';
+import { Header } from './Header';
+const routing =(
+  <BrowserRouter>  
+     <React.StrictMode>
+    <Header/>
+    <Routes>
+        <Route  path="/" element={ <App /> } />
+        <Route path="/post/:id" element={<FullPost/>} /> 
+    </Routes>
+   
+
   </React.StrictMode>
+  </BrowserRouter>
+
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(routing, document.getElementById('root'));
